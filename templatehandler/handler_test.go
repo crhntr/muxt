abb81fb76477188399d6e5b6c39a4de6c73280cc
Chrstopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-08-04 16:21:37 -0700

    refactor(templatehandler): remove internal tests

--- templatehandler/handler_test.go
+++ templatehandler/handler_test.go
@@ -633,3 +633,117 @@ type errorWriter struct {
 func (w errorWriter) Write([]byte) (int, error) {
 	return 0, fmt.Errorf("banna")
 }
+
+func Test_endpoint(t *testing.T) {
+	for _, tt := range []struct {
+		Name         string
+		TemplateName string
+		ExpMatch     bool
+		Pattern      func(t *testing.T, pat templatehandler.EndpointDefinition)
+		Error        func(t *testing.T, err error)
+	}{
+		{
+			Name:         "get root",
+			TemplateName: "GET /",
+			ExpMatch:     true,
+			Pattern: func(t *testing.T, pat templatehandler.EndpointDefinition) {
+				assert.Equal(t, templatehandler.EndpointDefinition{
+					Method:  http.MethodGet,
+					Host:    "",
+					Path:    "/",
+					Pattern: "GET /",
+					Handler: "",
+				}, pat)
+			},
+		},
+		{
+			Name:         "multiple spaces after method",
+			TemplateName: "GET  /",
+			ExpMatch:     true,
+			Pattern: func(t *testing.T, pat templatehandler.EndpointDefinition) {
+				assert.Equal(t, templatehandler.EndpointDefinition{
+					Method:  http.MethodGet,
+					Host:    "",
+					Path:    "/",
+					Pattern: "GET  /",
+					Handler: "",
+				}, pat)
+			},
+		},
+		{
+			Name:         "post root",
+			TemplateName: "POST /",
+			ExpMatch:     true,
+			Pattern: func(t *testing.T, pat templatehandler.EndpointDefinition) {
+				assert.Equal(t, templatehandler.EndpointDefinition{
+					Method:  http.MethodPost,
+					Host:    "",
+					Path:    "/",
+					Pattern: "POST /",
+					Handler: "",
+				}, pat)
+			},
+		},
+		{
+			Name:         "patch root",
+			TemplateName: "PATCH /",
+			ExpMatch:     true,
+			Pattern: func(t *testing.T, pat templatehandler.EndpointDefinition) {
+				assert.Equal(t, templatehandler.EndpointDefinition{
+					Method:  http.MethodPatch,
+					Host:    "",
+					Path:    "/",
+					Pattern: "PATCH /",
+					Handler: "",
+				}, pat)
+			},
+		},
+		{
+			Name:         "delete root",
+			TemplateName: "DELETE /",
+			ExpMatch:     true,
+			Pattern: func(t *testing.T, pat templatehandler.EndpointDefinition) {
+				assert.Equal(t, templatehandler.EndpointDefinition{
+					Method:  http.MethodDelete,
+					Host:    "",
+					Path:    "/",
+					Pattern: "DELETE /",
+					Handler: "",
+				}, pat)
+			},
+		},
+		{
+			Name:         "put root",
+			TemplateName: "PUT /",
+			ExpMatch:     true,
+			Pattern: func(t *testing.T, pat templatehandler.EndpointDefinition) {
+				assert.Equal(t, templatehandler.EndpointDefinition{
+					Method:  http.MethodPut,
+					Host:    "",
+					Path:    "/",
+					Pattern: "PUT /",
+					Handler: "",
+				}, pat)
+			},
+		},
+		{
+			Name:         "put root",
+			TemplateName: "OPTIONS /",
+			ExpMatch:     true,
+			Error: func(t *testing.T, err error) {
+				assert.ErrorContains(t, err, "OPTIONS method not allowed")
+			},
+		},
+	} {
+		t.Run(tt.Name, func(t *testing.T) {
+			pat, err, match := templatehandler.NewEndpointDefinition(tt.TemplateName)
+			require.Equal(t, tt.ExpMatch, match)
+			if tt.Error != nil {
+				tt.Error(t, err)
+			} else if tt.Pattern != nil {
+				assert.NoError(t, err)
+				tt.Pattern(t, pat)
+			}
+		})
+	}
+}
