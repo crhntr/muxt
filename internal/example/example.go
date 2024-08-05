fa3c657d92e874aa06145950bd26b60b7131dc69
Chrstopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-08-04 21:25:09 -0700

    refactor: remove template packages

--- internal/example/example.go
+++ internal/example/example.go
@@ -1,17 +1,5 @@
 package example
 
-type Numbers struct {
-	Rows []int
-}
-
-type Website struct {
-	Name string
-}
-
-func Function() {}
-
-var Variable = 0
-
 type Article struct {
 	ID       int
 	Title    string
