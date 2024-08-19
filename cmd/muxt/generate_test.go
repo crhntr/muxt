14921699ef5192324122cc54d0be088cbb803e68
Chrstopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-08-18 18:28:53 -0700

    test flag values

--- cmd/muxt/generate_test.go
+++ cmd/muxt/generate_test.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"rsc.io/script"
 	"rsc.io/script/scripttest"
 )
@@ -43,4 +44,31 @@ func TestGenerate(t *testing.T) {
 			scripttest.Test(t, ctx, e, nil, filePath)
 		})
 	}
+}
+
+func Test_newGenerate(t *testing.T) {
+	t.Run("unknown flag", func(t *testing.T) {
+		_, err := newGenerate([]string{
+			"--unknown",
+		}, func(s string) string { return "" })
+		assert.ErrorContains(t, err, "flag provided but not defined")
+	})
+	t.Run(receiverStaticType+" flag value is an invalid identifier", func(t *testing.T) {
+		_, err := newGenerate([]string{
+			"--" + receiverStaticType, "123",
+		}, func(s string) string { return "" })
+		assert.ErrorContains(t, err, errIdentSuffix)
+	})
+	t.Run(routesFunc+" flag value is an invalid identifier", func(t *testing.T) {
+		_, err := newGenerate([]string{
+			"--" + routesFunc, "123",
+		}, func(s string) string { return "" })
+		assert.ErrorContains(t, err, errIdentSuffix)
+	})
+	t.Run(templatesVariable+" flag value is an invalid identifier", func(t *testing.T) {
+		_, err := newGenerate([]string{
+			"--" + templatesVariable, "123",
+		}, func(s string) string { return "" })
+		assert.ErrorContains(t, err, errIdentSuffix)
+	})
 }
