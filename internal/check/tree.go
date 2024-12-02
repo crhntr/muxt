4e1c8aee861c16c529c806ee04835f344d32d14b
Chrstopher Hunter <8398225+crhntr@users.noreply.github.com>
2024-12-01 17:34:28 -0800

    type check key value range variables

--- internal/check/tree.go
+++ internal/check/tree.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"go/token"
 	"go/types"
+	"maps"
 	"text/template/parse"
 )
 
@@ -17,16 +18,21 @@ type FunctionFinder interface {
 
 func Tree(tree *parse.Tree, data types.Type, pkg *types.Package, fileSet *token.FileSet, trees TreeFinder, functions FunctionFinder) error {
 	s := &scope{
-		TreeFinder:     trees,
-		FunctionFinder: functions,
-		pkg:            pkg,
-		fileSet:        fileSet,
+		global: global{
+			TreeFinder:     trees,
+			FunctionFinder: functions,
+			pkg:            pkg,
+			fileSet:        fileSet,
+		},
+		variables: map[string]types.Type{
+			"$": data,
+		},
 	}
-	_, err := s.typeCheckNode(tree, data, tree.Root)
+	_, err := s.checkNode(tree, data, tree.Root)
 	return err
 }
 
-type scope struct {
+type global struct {
 	TreeFinder
 	FunctionFinder
 
@@ -34,7 +40,12 @@ type scope struct {
 	fileSet *token.FileSet
 }
 
-func (s *scope) typeCheckNode(tree *parse.Tree, dot types.Type, node parse.Node) (types.Type, error) {
+type scope struct {
+	global
+	variables map[string]types.Type
+}
+
+func (s *scope) checkNode(tree *parse.Tree, dot types.Type, node parse.Node) (types.Type, error) {
 	switch n := node.(type) {
 	case *parse.DotNode:
 		return dot, nil
@@ -50,6 +61,8 @@ func (s *scope) typeCheckNode(tree *parse.Tree, dot types.Type, node parse.Node)
 		return s.checkPipeNode(tree, dot, n)
 	case *parse.IfNode:
 		return s.checkIfNode(tree, dot, n)
+	case *parse.RangeNode:
+		return s.checkRangeNode(tree, dot, n)
 	case *parse.TemplateNode:
 		return s.checkTemplateNode(tree, dot, n)
 	case *parse.BoolNode:
@@ -58,16 +71,27 @@ func (s *scope) typeCheckNode(tree *parse.Tree, dot types.Type, node parse.Node)
 		return types.Typ[types.UntypedString], nil
 	case *parse.NumberNode:
 		return newNumberNodeType(n)
+	case *parse.VariableNode:
+		return s.checkVariableNode(tree, n)
+	case *parse.IdentifierNode:
+		return s.checkIdentifierNode(n)
 	case *parse.TextNode:
 		return nil, nil
 	default:
 		return nil, fmt.Errorf("missing node type check %T", n)
 	}
 }
+func (s *scope) checkVariableNode(tree *parse.Tree, n *parse.VariableNode) (types.Type, error) {
+	tp, ok := s.variables[n.Ident[0]]
+	if !ok {
+		return nil, fmt.Errorf("variable %s not found", n.Ident[0])
+	}
+	return s.checkIdentifiers(tree, tp, n, n.Ident[1:])
+}
 
 func (s *scope) checkListNode(tree *parse.Tree, dot types.Type, n *parse.ListNode) (types.Type, error) {
 	for _, child := range n.Nodes {
-		if _, err := s.typeCheckNode(tree, dot, child); err != nil {
+		if _, err := s.checkNode(tree, dot, child); err != nil {
 			return nil, err
 		}
 	}
@@ -76,7 +100,7 @@ func (s *scope) checkListNode(tree *parse.Tree, dot types.Type, n *parse.ListNod
 
 func (s *scope) checkActionNode(tree *parse.Tree, dot types.Type, n *parse.ActionNode) (types.Type, error) {
 	for _, cmd := range n.Pipe.Cmds {
-		if _, err := s.typeCheckNode(tree, dot, cmd); err != nil {
+		if _, err := s.checkNode(tree, dot, cmd); err != nil {
 			return nil, err
 		}
 	}
@@ -86,21 +110,55 @@ func (s *scope) checkActionNode(tree *parse.Tree, dot types.Type, n *parse.Actio
 func (s *scope) checkPipeNode(tree *parse.Tree, dot types.Type, n *parse.PipeNode) (types.Type, error) {
 	x := dot
 	for _, cmd := range n.Cmds {
-		tp, err := s.typeCheckNode(tree, x, cmd)
+		tp, err := s.checkNode(tree, x, cmd)
 		if err != nil {
 			return nil, err
 		}
 		x = tp
 	}
+	if len(n.Decl) > 0 {
+		switch r := x.(type) {
+		case *types.Slice:
+			if l := len(n.Decl); l == 1 {
+				s.variables[n.Decl[0].Ident[0]] = r.Elem()
+			} else if l == 2 {
+				s.variables[n.Decl[0].Ident[0]] = types.Typ[types.Int]
+				s.variables[n.Decl[1].Ident[0]] = r.Elem()
+			} else {
+				return nil, fmt.Errorf("expected 1 or 2 declaration")
+			}
+		case *types.Array:
+			if l := len(n.Decl); l == 1 {
+				s.variables[n.Decl[0].Ident[0]] = r.Elem()
+			} else if l == 2 {
+				s.variables[n.Decl[0].Ident[0]] = types.Typ[types.Int]
+				s.variables[n.Decl[1].Ident[0]] = r.Elem()
+			} else {
+				return nil, fmt.Errorf("expected 1 or 2 declaration")
+			}
+		case *types.Map:
+			if l := len(n.Decl); l == 1 {
+				s.variables[n.Decl[0].Ident[0]] = r.Elem()
+			} else if l == 2 {
+				s.variables[n.Decl[0].Ident[0]] = r.Key()
+				s.variables[n.Decl[1].Ident[0]] = r.Elem()
+			} else {
+				return nil, fmt.Errorf("expected 1 or 2 declaration")
+			}
+		}
+	}
 	return x, nil
 }
 
 func (s *scope) checkIfNode(tree *parse.Tree, dot types.Type, n *parse.IfNode) (types.Type, error) {
-	tp, err := s.typeCheckNode(tree, dot, n.Pipe)
+	_, err := s.checkNode(tree, dot, n.Pipe)
 	if err != nil {
 		return nil, err
 	}
-	return tp, nil
+	if _, err := s.checkNode(tree, dot, n.List); err != nil {
+		return nil, err
+	}
+	return nil, nil
 }
 
 func newNumberNodeType(n *parse.NumberNode) (types.Type, error) {
@@ -122,7 +180,7 @@ func newNumberNodeType(n *parse.NumberNode) (types.Type, error) {
 func (s *scope) checkTemplateNode(tree *parse.Tree, dot types.Type, n *parse.TemplateNode) (types.Type, error) {
 	x := dot
 	if n.Pipe != nil {
-		tp, err := s.typeCheckNode(tree, x, n.Pipe)
+		tp, err := s.checkNode(tree, x, n.Pipe)
 		if err != nil {
 			return nil, err
 		}
@@ -130,16 +188,51 @@ func (s *scope) checkTemplateNode(tree *parse.Tree, dot types.Type, n *parse.Tem
 	} else {
 		x = types.Typ[types.UntypedNil]
 	}
-	child, ok := s.FindTree(n.Name)
+	childTree, ok := s.FindTree(n.Name)
 	if !ok {
 		return nil, fmt.Errorf("template %q not found", n.Name)
 	}
-	return s.typeCheckNode(child, x, n.Pipe)
+	childScope := scope{
+		global: s.global,
+		variables: map[string]types.Type{
+			"$": x,
+		},
+	}
+	return childScope.checkNode(childTree, x, childTree.Root)
 }
 
 func (s *scope) checkFieldNode(tree *parse.Tree, dot types.Type, n *parse.FieldNode) (types.Type, error) {
+	return s.checkIdentifiers(tree, dot, n, n.Ident)
+}
+
+func (s *scope) checkCommandNode(tree *parse.Tree, dot types.Type, n *parse.CommandNode) (types.Type, error) {
+	var argTypes []types.Type
+	for _, arg := range n.Args {
+		argType, err := s.checkNode(tree, dot, arg)
+		if err != nil {
+			return nil, err
+		}
+		argTypes = append(argTypes, argType)
+	}
+	cmd := argTypes[0]
+	argTypes = argTypes[1:]
+	sig, ok := cmd.(*types.Signature)
+	if !ok {
+		return cmd, nil
+	}
+	for i := 0; i < len(argTypes); i++ {
+		at := argTypes[i]
+		pt := sig.Params().At(i).Type()
+		if !types.AssignableTo(at, pt) {
+			return nil, fmt.Errorf("%s argument %d has type %s expected %s", n.Args[0], i-1, at, pt)
+		}
+	}
+	return sig.Results().At(0).Type(), nil
+}
+
+func (s *scope) checkIdentifiers(tree *parse.Tree, dot types.Type, n parse.Node, idents []string) (types.Type, error) {
 	x := dot
-	for i, ident := range n.Ident {
+	for i, ident := range idents {
 		obj, _, _ := types.LookupFieldOrMethod(x, true, nil, ident)
 		if obj == nil {
 			loc, _ := tree.ErrorContext(n)
@@ -165,12 +258,12 @@ func (s *scope) checkFieldNode(tree *parse.Tree, dot types.Type, n *parse.FieldN
 					return nil, fmt.Errorf("type check failed: %s: invalid function signature for %s: second return value should be error; is %s: incorrect signature at %s", loc, ident, finalResult.Type(), methodPos)
 				}
 			}
-			if i == len(n.Ident)-1 {
+			if i == len(idents)-1 {
 				return o.Type(), nil
 			}
 			x = sig.Results().At(0).Type()
 		}
-		if _, ok := x.(*types.Signature); ok && i < len(n.Ident)-1 {
+		if _, ok := x.(*types.Signature); ok && i < len(idents)-1 {
 			loc, _ := tree.ErrorContext(n)
 			return nil, fmt.Errorf("type check failed: %s: can't evaluate field %s in type %s", loc, ident, x)
 		}
@@ -178,28 +271,41 @@ func (s *scope) checkFieldNode(tree *parse.Tree, dot types.Type, n *parse.FieldN
 	return x, nil
 }
 
-func (s *scope) checkCommandNode(tree *parse.Tree, dot types.Type, n *parse.CommandNode) (types.Type, error) {
-	var argTypes []types.Type
-	for _, arg := range n.Args {
-		argType, err := s.typeCheckNode(tree, dot, arg)
-		if err != nil {
-			return nil, err
-		}
-		argTypes = append(argTypes, argType)
+func (s *scope) checkRangeNode(tree *parse.Tree, dot types.Type, n *parse.RangeNode) (types.Type, error) {
+	loopScope := &scope{
+		global:    s.global,
+		variables: maps.Clone(s.variables),
 	}
-	if len(n.Args) > 1 {
-		cmd := argTypes[0]
-		argTypes = argTypes[1:]
-
-		sig := cmd.(*types.Signature)
-
-		for i := 0; i < len(argTypes); i++ {
-			at := argTypes[i]
-			pt := sig.Params().At(i).Type()
-			if !types.AssignableTo(at, pt) {
-				return nil, fmt.Errorf("%s argument %d has type %s expected %s", n.Args[0], i-1, at, pt)
-			}
+	pipeType, err := loopScope.checkNode(tree, dot, n.Pipe)
+	if err != nil {
+		return nil, err
+	}
+	var x types.Type
+	switch pt := pipeType.(type) {
+	case *types.Slice:
+		x = pt.Elem()
+	case *types.Array:
+		x = pt.Elem()
+	case *types.Map:
+		x = pt.Elem()
+	default:
+		return nil, fmt.Errorf("failed to range over %s", pipeType)
+	}
+	if _, err := loopScope.checkNode(tree, x, n.List); err != nil {
+		return nil, err
+	}
+	if n.ElseList != nil {
+		if _, err := loopScope.checkNode(tree, x, n.ElseList); err != nil {
+			return nil, err
 		}
 	}
 	return nil, nil
+}
+
+func (s *scope) checkIdentifierNode(n *parse.IdentifierNode) (types.Type, error) {
+	tp, ok := s.variables[n.Ident]
+	if !ok {
+		return nil, fmt.Errorf("failed to find identifier %q", n.Ident)
+	}
+	return tp, nil
 }
